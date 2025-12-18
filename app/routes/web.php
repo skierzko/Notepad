cde5d2f41576f6f38085c070e68c0c619d158e6c
/ <skierzko@gmail.com>
2025-12-18 16:10:53 +0100

    Added notes route

--- app/routes/web.php
+++ app/routes/web.php
@@ -9,11 +9,15 @@ Route::get('/', function () {
         return redirect('/login');
     }
 
-    return redirect('/dashboard');
+    return redirect('/notes');
 })->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('notes', function () {
+    return Inertia::render('NotesMain');
+})->middleware(['auth', 'verified'])->name('notes');
 
 require __DIR__.'/settings.php';
