4dcd14a08a9051431aef023d231940cae20165f9
/ <skierzko@gmail.com>
2025-12-18 16:11:42 +0100

    Added notes components

---
+++ app/resources/js/pages/notes/NotesNotesList.vue
@@ -0,0 +1,26 @@
+<script setup lang="js">
+import { defineProps } from 'vue';
+import NotesNotesRow from './NotesNotesRow.vue';
+import { Notebook } from 'lucide-vue-next';
+
+defineProps({
+    list: {
+        type: Array,
+        required: true,
+    }
+});
+</script>
+
+<template>
+    <div class="min-w-[200px] border border-gray-200 p-2">
+        <div class="font-bold">
+            <Notebook class="inline relative top-[-2px]" />
+            Notes
+        </div>
+
+        <hr />
+
+        <NotesNotesRow />
+        <NotesNotesRow />
+    </div>
+</template>
