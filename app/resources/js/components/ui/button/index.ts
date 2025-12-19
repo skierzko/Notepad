2f56d80e95187efa52e2c9ab9420975d8312b9b8
/ <skierzko@gmail.com>
2025-12-19 11:45:21 +0100

    Added primary  button variant

--- app/resources/js/components/ui/button/index.ts
+++ app/resources/js/components/ui/button/index.ts
@@ -10,6 +10,8 @@ export const buttonVariants = cva(
       variant: {
         default:
           "bg-primary text-primary-foreground hover:bg-primary/90",
+        primary:
+          "bg-emerald-600 text-white hover:bg-emerald-600/90",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
