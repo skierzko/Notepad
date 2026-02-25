fd3069f3891014e9ab7b807a1a7440d2d4bd0468
/ <skierzko@gmail.com>
2026-02-25 17:35:30 +0100

    Changed setting menu link in bottom menu for appearance

--- app/resources/js/components/UserMenuContent.vue
+++ app/resources/js/components/UserMenuContent.vue
@@ -32,7 +32,7 @@ defineProps<Props>();
     <DropdownMenuSeparator />
     <DropdownMenuGroup>
         <DropdownMenuItem :as-child="true">
-            <Link class="block w-full" :href="edit()" prefetch as="button">
+            <Link class="block w-full" href="/settings/appearance" prefetch as="button">
                 <Settings class="mr-2 h-4 w-4" />
                 Settings
             </Link>
